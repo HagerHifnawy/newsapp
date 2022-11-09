986320f430a5fc997472e9fb2448d1d1af718f25
HagerHifnawy <66131843+HagerHifnawy@users.noreply.github.com>
2022-11-09 19:49:59 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
+
+import 'news_app/business_logic/bloc_observer.dart';
+
+void main() {
+  Bloc.observer = MyBlocObserver();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      //home: ,
+    );
+  }
+}
